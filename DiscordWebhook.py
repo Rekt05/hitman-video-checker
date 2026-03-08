be4b73b392d5a05949dcaffed3cf1d10c951427d
Rekt05 <coetzeereinhardt.rc@gmail.com>
2026-03-08 18:52:08 +0200

    add checking to only send webhook messages when the count has changed

--- DiscordWebhook.py
+++ DiscordWebhook.py
@@ -4,21 +4,36 @@ import os
 
 webhook = os.getenv('dcwebhook')
 file = 'unavailable_yt_videos.json'
+txt = 'unavailable_vid_count.txt'
 
 def send_message():
     with open(file, 'r') as f:
         data = json.load(f)
         count = len(data)
 
-    if count > 0:
+    last_count = 0
+    with open(txt, 'r') as f:
+        content = f.read().strip()
+        last_count = int(content) if content else 0
+
+    if count != last_count:
+        diff = count - last_count
+        if diff > 0:
+            desc = f"Found {diff} new unavailable video(s) - Total {count}"
+        else:
+            desc = f"{abs(diff)} less unavailable video(s) - Total {count}"
         message = {
             "embeds": [{
-                "title": "New Unavailable Videos",
-                "description": f"Found {count} new unavailable video(s)",
+                "title": "Unavailable Videos Update",
+                "description": desc,
                 "url": "https://rekt05.github.io/hitman-video-checker/"
             }]
         }
-        requests.post(webhook, json=message)
+        response = requests.post(webhook, json=message)
+
+        if response.status_code < 300:
+            with open(txt, 'w') as f:
+                f.write(str(count))
 
 if __name__ == "__main__":
     send_message()
